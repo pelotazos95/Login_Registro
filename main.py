28ac1c0c9deba12f204e22bacd9bce1989064d18
pelotazos95 <pelotasgomez7@gmail.com>
2025-04-05 12:00:02 +0200

    terminar siguiente pag parte 2

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ def main(page: ft.Page):
     page.go("/login")
 
 if __name__ == "__main__":
-    ft.app(target=main, view=ft.WEB_BROWSER, port=30018)
+    ft.app(target=main, view=ft.WEB_BROWSER, port=30018, host="0.0.0.0")
